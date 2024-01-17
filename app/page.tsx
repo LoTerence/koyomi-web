4fbddbb5b363d395308d74db2d82f5f14060d77c
Terence Lo <terencelomoa@gmail.com>
2024-01-16 18:54:09 -0800

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 import Link from 'next/link';
 import { ModeToggle } from '@/components/ui/mode-toggle';
 import { ArrowRight } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <section className="text-lg">
-        <p className="mt-6 leading-7">
+      <section className="mt-6 text-lg">
+        <p>
           Welcome to the <b>Koyomi web app</b>! This will become an ADHD helper
           app
         </p>
-        <p className="mt-6 leading-7">Brought to you by CS Studio.</p>
-        <p className="mt-6 leading-7">Log in to get started!</p>
+        <p className="mt-6">Brought to you by CS Studio</p>
       </section>
       <Link
         href="/login"
-        className="mt-6 flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        className={cn(
+          'mt-6 flex items-center gap-5 self-start',
+          buttonVariants({ variant: 'default' }),
+        )}
       >
-        <span>Log in</span> <ArrowRight />
+        <span>Log in to get started</span> <ArrowRight />
       </Link>
       <div className="mt-6">
         <ModeToggle />
