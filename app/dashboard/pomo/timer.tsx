4f8547f4cb75a255538066c24e4f05706f710a92
Terence Lo <terencelomoa@gmail.com>
2024-01-27 02:09:16 -0800

    style session button group

--- app/dashboard/pomo/timer.tsx
+++ app/dashboard/pomo/timer.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import usePomo, { sessions, formatTime, Session } from './usePomo';
 import { Button } from '@/components/ui/button';
+import clsx from 'clsx';
 
 export default function Timer() {
   const [activeSessionId, setActiveSessionId] = useState(0);
@@ -18,23 +19,23 @@ export default function Timer() {
   return (
     <section>
       {/* Session Button Group */}
-      <div className="mx-auto flex w-full max-w-lg shadow-[0_4px_9px_-4px_#3b71ca]">
+      <div className="mx-auto flex w-full max-w-xl shadow-[0_4px_9px_-4px_#3b71ca]">
         {sessions.map((session) => {
           const isActive = activeSessionId === session.id;
           return (
-            <button
+            <Button
               key={session.id}
               type="button"
-              className={
-                (isActive
-                  ? 'bg-primary-800 dark:bg-primary-700 '
-                  : 'hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 bg-primary focus:outline-none focus:ring-0 ') +
-                'border-primary-800 inline-block flex-1 border  px-4 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white transition duration-150 ease-in-out'
-              }
+              className={clsx(
+                'h-12 flex-1 rounded-none border border-input px-4 uppercase text-white',
+                isActive
+                  ? 'bg-sky-900 hover:bg-sky-900'
+                  : 'bg-sky-600 hover:bg-sky-600/90 active:bg-sky-700',
+              )}
               onClick={() => handleSessionButtonClick(session)}
             >
               {session.name}
-            </button>
+            </Button>
           );
         })}
       </div>
@@ -50,7 +51,7 @@ export default function Timer() {
       </div>
 
       {/* Timer Controls */}
-      <div className="mx-auto flex w-full max-w-lg flex-wrap justify-center gap-x-4 sm:flex-nowrap">
+      <div className="mx-auto flex w-full max-w-xl flex-wrap justify-center gap-x-4 sm:flex-nowrap">
         <Button
           size="lg"
           className="w-full bg-green-600 text-xl text-white hover:bg-green-600/90 sm:flex-1"
