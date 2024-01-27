9cf6ab65b73f2f8c8988efb94c3bea1267b399a1
Terence Lo <terencelomoa@gmail.com>
2024-01-27 01:32:24 -0800

    timer control buttons

--- app/dashboard/pomo/timer.tsx
+++ app/dashboard/pomo/timer.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import usePomo, { sessions, formatTime, Session } from './usePomo';
+import { Button } from '@/components/ui/button';
 
 export default function Timer() {
   const [activeSessionId, setActiveSessionId] = useState(0);
@@ -39,40 +40,40 @@ export default function Timer() {
       </div>
 
       {/* Timer Face */}
-      <div className="my-4">
-        <div className="flex flex-col">
-          <div className="mx-auto text-xl">
-            {sessions[activeSessionId].minutes} mins
-          </div>
-          <div className="mx-auto text-[6rem] font-bold">
-            {formatTime(timeLeft)}
-          </div>
+      <div className="my-4 flex flex-col">
+        <div className="mx-auto text-xl">
+          {sessions[activeSessionId].minutes} mins
+        </div>
+        <div className="mx-auto text-[6rem] font-bold">
+          {formatTime(timeLeft)}
         </div>
       </div>
 
       {/* Timer Controls */}
       <div className="mx-auto flex w-full max-w-lg flex-wrap justify-center gap-x-4 sm:flex-nowrap">
-        <button
-          type="button"
-          className="text-md bg-success hover:bg-success-600 focus:bg-success-600 active:bg-success-700 inline-block w-full rounded px-6 pb-2 pt-2.5 font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] sm:flex-1"
+        <Button
+          size="lg"
+          className="w-full bg-green-600 text-xl text-white hover:bg-green-600/90 sm:flex-1"
           onClick={() => startTimer()}
         >
-          Start
-        </button>
-        <button
-          type="button"
-          className="text-md bg-danger hover:bg-danger-600 focus:bg-danger-600 active:bg-danger-700 inline-block w-full rounded px-6 pb-2 pt-2.5 font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] sm:flex-1"
+          START
+        </Button>
+        <Button
+          size="lg"
+          variant="destructive"
+          className="w-full text-xl sm:flex-1"
           onClick={() => stopTimer()}
         >
-          Stop
-        </button>
-        <button
-          type="button"
-          className="text-md hover:bg-secondary-600 focus:bg-secondary-600 active:bg-secondary-700 inline-block w-full rounded bg-secondary px-6 pb-2 pt-2.5 font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] sm:flex-1"
+          STOP
+        </Button>
+        <Button
+          size="lg"
+          variant="secondary"
+          className="w-full text-xl sm:flex-1"
           onClick={() => resetTimer()}
         >
-          Reset
-        </button>
+          RESET
+        </Button>
       </div>
     </section>
   );
