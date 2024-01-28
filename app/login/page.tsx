58eaf6da51900ec2d56ad99b2fe9ff62a51cf28e
Terence Lo <terencelomoa@gmail.com>
2024-01-27 19:21:59 -0800

    change main to div

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <div className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-16 w-full items-end rounded-lg bg-slate-900 p-3 md:h-20">
           <Link href="/">
@@ -18,6 +18,6 @@ export default function Page() {
         </div>
         <LoginForm />
       </div>
-    </main>
+    </div>
   );
 }
