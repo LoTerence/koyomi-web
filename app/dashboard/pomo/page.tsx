4494f18398f2ec0b0f811cec1f5074f55525bc87
Terence Lo <terencelomoa@gmail.com>
2024-01-27 20:04:09 -0800

    heading style

--- app/dashboard/pomo/page.tsx
+++ app/dashboard/pomo/page.tsx
@@ -1,12 +1,11 @@
+import utilStyles from '@/styles/utils.module.css';
 import Timer from './timer';
 
 export default function Page() {
   return (
     <>
-      <section className="mt-4">
-        <h2 className="mb-2 mt-0 text-4xl font-medium leading-tight">
-          Pomodoro Timer
-        </h2>
+      <section>
+        <h1 className={utilStyles.headingX1}>Pomodoro Timer</h1>
       </section>
       <Timer />
     </>
